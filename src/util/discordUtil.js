7156fc823fd3627348168582d62bd2533e3bbb12
Michael Standen <screaminghawk@gmail.com>
2021-04-23 06:48:43 +1200

    Add logging to detect message without member

--- src/util/discordUtil.js
+++ src/util/discordUtil.js
@@ -1,8 +1,23 @@
+const log = require('./logger')
+
 const checkAdmin = (message, alert = true) => {
 	if (!message.guild) {
 		// DM. Assume admin
 		return true
 	}
+	//FIXME This is bug testing code
+	if (!message.member) {
+		log.error(
+			'For some reason the message has no member... Need to investigate',
+		)
+		try {
+			log.error(JSON.stringify(message, null, 2))
+		} catch (err) {
+			log.error('Unable to stringify message', err)
+		}
+		return false
+	}
+	// Check admin status
 	if (!message.member.hasPermission('ADMINISTRATOR')) {
 		if (alert) {
 			message.reply('only administators can do this')
